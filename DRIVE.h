46fcd202122bc7591486f0b2c74f0fe79c67ee1b
Omjd12 <omjadha9728@gmail.com>
2026-04-18 16:39:51 +0530

    Refactor MOTOR class and update movement methods
    Refactor MOTOR class to motor, update methods for forward movement and PWM control.

--- DRIVE.h
+++ DRIVE.h
@@ -1,9 +1,20 @@
-#ifndef VAL
-#define VAL
+#ifndef DRIVE_H
+#define DRIVE_H
 
-class MOTOR {
+
+class motor {
+private:
+  uint8_t a1, a2, b1, b2, pwm1, pwm2;
+  int16_t pwmL, pwmR;
+  uint8_t baseSpeedFr = 200;
+  uint8_t baseSpeedBk = 150;
+  int8_t correctionLeft = 0;
+  int8_t correctionRight = 0;
+
+
+
 public:
-  MOTOR(uint8_t x1, uint8_t x2, uint8_t y1, uint8_t y2) {
+  motor(uint8_t x1, uint8_t x2, uint8_t y1, uint8_t y2) {
     a1 = x1;
     a2 = x2;
     b2 = y2;
@@ -14,7 +25,6 @@ public:
     pinMode(b1, OUTPUT);
     pinMode(b2, OUTPUT);
   }
-
   void setPWM(uint8_t p1, uint8_t p2) {
     pwm1 = p1;
     pwm2 = p2;
@@ -23,38 +33,21 @@ public:
     pinMode(pwm2, OUTPUT);
   }
 
-  void Run(int16_t value) {
-    if (value < 0) {
-      anticlockWise();
-      pwm = -1 * value;
-    } else {
-      clockWise();
-      pwm = value;
-    }
-      
-    analogWrite(pwm1, pwm);
-    analogWrite(pwm2, pwm);
-  }
-  void stop() {
-    digitalWrite(a1, LOW);
-    digitalWrite(a2, LOW);
-    digitalWrite(b1, LOW);
-    digitalWrite(b2, LOW);
-  }
-
-private:
-  uint8_t a1, a2, b1, b2, pwm1, pwm2, pwm;
-  void anticlockWise() {
-    digitalWrite(a1, HIGH);
-    digitalWrite(a2, LOW);
-    digitalWrite(b1, LOW);
-    digitalWrite(b2, HIGH);
-  }
-  void clockWise() {
+  void forward(int8_t &output, float &error) {
     digitalWrite(a1, LOW);
     digitalWrite(a2, HIGH);
-    digitalWrite(b1, HIGH);
-    digitalWrite(b2, LOW);
+    digitalWrite(b1, LOW);  
+    digitalWrite(b2, HIGH);
+      correctionLeft = -(output);
+      correctionRight = output;
+    
+    analogWrite(pwm1, (baseSpeedFr + correctionLeft));
+    analogWrite(pwm2, (baseSpeedFr + correctionRight));
+    Serial.print(error); // its actually printing the angle since the target angle for forward movenent is 0.00
+    Serial.print("\t");
+    Serial.print(correctionLeft);
+    Serial.print("\t");
+    Serial.println(correctionRight);
   }
 };
 #endif
